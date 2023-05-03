98cbe78f19c802e866d4679b428763d9d10a172d
subsub <zzzzk9113@gmail.com>
2023-05-03 10:04:06 +0900

    .
    .

---
+++ src/TodoHead.js
@@ -0,0 +1,32 @@
+import reate from 'react';
+import styled from 'styled-components';
+
+const TodoHeadBlock = styled.div`
+    padding-top: 48px;
+    padding-left: 32px;
+    padding-right: 32px;
+    padding-bottom: 24px;
+    border-bottom: 1px solid #e9ecef;
+    .day{
+        margin-top: 4px;
+        font-size: 21px;
+        color: #868e96;
+    }
+    .tasks-left{
+        margin-top: 40px;
+        font-size: 18px;
+        color: #20c997;
+        font-weight: bold; 
+    }
+`
+function TodoHead(){
+    return(
+        <TodoHeadBlock>
+            <h1>2023년 5월 3일</h1>
+            <div className='day'>수요일</div>
+            <div className='tasks-left'>할 일 2개 남음</div>
+        </TodoHeadBlock>
+    )
+}
+
+export default TodoHead;
